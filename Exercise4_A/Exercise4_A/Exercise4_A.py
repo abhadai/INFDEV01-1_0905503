8a964d8c543096771d21ba9892f7b26ae4054e32
Andy Bhadai <abhadai@gmail.com>
2015-09-23 11:47:03 +0200

    Exercise 4A added

---
+++ Exercise4_A/Exercise4_A/Exercise4_A.py
@@ -0,0 +1,11 @@
+# Get user input
+fahrenheit = input("Type a number to convert to Celsius:")
+
+#Convert user input to float
+fahrenheit = float(fahrenheit)
+
+#Calculate Celsius
+celsius = round((fahrenheit - 32) * 5 /9)
+
+# Convert float to string and give user feedback
+print(str(fahrenheit) + " Fahrenheit is " + str(celsius) + " Celsius")
