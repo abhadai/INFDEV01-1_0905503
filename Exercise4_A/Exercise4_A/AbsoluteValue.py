0603d07947338d286dc82a26066711ba8fb2760c
Andy Bhadai <abhadai@gmail.com>
2015-09-23 12:28:05 +0200

    Added exercise 1C

---
+++ Exercise4_A/Exercise4_A/AbsoluteValue.py
@@ -0,0 +1,11 @@
+# Get user input
+number = input("Please type a number and press ENTER")
+
+# Convert number to absolute value
+number = abs(float(number))
+
+# Give user feedback
+print("The absolute value is " + str(number))
+
+
+ 
--- Exercise4_A/Exercise4_A/CelsiusToKelvin.py
+++ Exercise4_A/Exercise4_A/CelsiusToKelvin.py
@@ -4,7 +4,5 @@ celsius = input("Type a number to convert to Kelvin")
 # Convert Celsius to Kelvin
 kelvin = float(celsius) + 273.15
 
-if kelvin < 0:
-
 # Give user feedback
 print(str(celsius) + " Celsius is " + str(kelvin) + " Kelvin") 
