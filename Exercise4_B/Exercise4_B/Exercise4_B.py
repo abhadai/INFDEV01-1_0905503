88b3c6a153e1eeff12f1ac5fcd0824fc4079ec9c
Andy Bhadai <abhadai@gmail.com>
2015-09-26 19:10:58 +0200

    Restructured RPS game and added turtle exercise to Git

--- Exercise4_B/Exercise4_B/Exercise4_B.py
+++ Exercise4_B/Exercise4_B/Exercise4_B.py
@@ -1,44 +1,44 @@
-import random
+﻿while True:
+    # Ask player 1 for input
+    player_one_choice = input("Player 1, type R for rock, P for paper or S for scissors!")
+    player_one_choice = player_one_choice.capitalize()
 
-# The choices in the game
-choices = ["rock", "paper", "scissors"]
-
-# Ask the users for their choice
-player_one_choice = input("Player 1, type rock, paper or scissors\n\r")
-
-# If choice exists, continue game
-if(player_one_choice in choices):
-    print("CHOICE FOUND" + player_one_choice)
+    # Ask player 2 for input
+    player_two_choice = input("Player 2, type R for rock, P for paper or S for scissors!")
+    player_two_choice = player_two_choice.capitalize()
 
     # Rock and rock ties
-    if(player_one_choice == "rock" and player_two_choice == "rock"):
-        print("No one wins!")
+    if(player_one_choice == "R" and player_two_choice == "R"):
+        print("No one wins! Both rocks evolved into one Onix.")
 
     # Paper beats rock
-    if(player_one_choice == "rock" and player_two_choice == "paper"):
-        print("P2 wins!")
+    if(player_one_choice == "R" and player_two_choice == "P"):
+        print("P2 wins! Paper wrinkled itself around rock.")
     
     # Rock beats scissors      
-    if(player_one_choice == "rock" and player_two_choice == "scissors"):
-        print("P1 wins")
+    if(player_one_choice == "R" and player_two_choice == "S"):
+        print("P1 wins! Scissors broke itself on the rock.")
     
     # Paper beats rock
-    if(player_one_choice == "paper" and player_two_choice == "rock"):
-        print("P1 wins!")
+    if(player_one_choice == "P" and player_two_choice == "R"):
+        print("P1 wins! Paper wrinkled itself around rock.")
+    
+    # Paper and paper ties
+    if(player_one_choice == "P" and player_two_choice == "P"):
+        print("No one wins! The two papers are now a newsarticle.")
 
-    if(player_one_choice == "paper" and player_two_choice == "paper"):
-        print("P2 wins!")
+    # Scissors beats paper
+    if(player_one_choice == "P" and player_two_choice == "S"):
+        print("P2 wins! Scissors cut the paper in half.")
 
-    if(player_one_choice == "paper" and player_two_choice == "scissors"):
-        print("P1 wins")
+    # Paper beats rock
+    if(player_one_choice == "S" and player_two_choice == "R"):
+        print("P2 wins! Rock crushed scissor.")
+    
+    # Paper and paper ties
+    if(player_one_choice == "S" and player_two_choice == "P"):
+        print("P1 wins! Scissor cut paper in half.")
 
-else:
-    # If choice doesn't exist, ask again
-    player_one_choice = input("Player 1 your choice was not found, type rock, paper or scissors\n\r")
-
-# If choice exists, continue game
-if(player_one_choice in choices):
-    print("CHOICE FOUND" + player_one_choice)
-else:
-    # If choice doesn't exist, ask again
-    player_two_choice = input("Player 2 your choice was not found, type rock, paper or scissors\n\r")
+    # Scissors beats paper
+    if(player_one_choice == "S" and player_two_choice == "S"):
+        print("No one wins! The two scissor lived happily ever after.")
