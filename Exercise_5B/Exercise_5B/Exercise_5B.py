01a30c9700ed0d84e4c49253c951ea3bb6bd9d98
Andy Bhadai <abhadai@gmail.com>
2015-10-05 17:02:13 +0200

    Improved password exercise

--- Exercise_5B/Exercise_5B/Exercise_5B.py
+++ Exercise_5B/Exercise_5B/Exercise_5B.py
@@ -5,13 +5,15 @@ feedback = []
 capital_count = 0
 symbol_count = 0
 number_count = 0
+spaces_count = 0
+special_character_count = 0
 special_chars = "!@#$%^&*()[];:'?<>.,"
 
 # Check password length
 if len(password) < 4:
     weak_password_count += 3
     feedback.append("Your password is too short!")
-elif len(password) > 10:
+elif len(password) > 12:
     strong_password_count += 5
 else:
     strong_password_count += 2
@@ -27,12 +29,13 @@ for p in password:
 
     if ord(p) == 32:
         spaces_count += 2
-        strong_password_count += 2
+        strong_password_count += 5
     
     # Check if special character is used
     for sc in special_chars:
         if p == sc:
             strong_password_count += 2
+            special_character_count += 1
 
 if capital_count == 0:
     weak_password_count += 1
@@ -40,18 +43,22 @@ if capital_count == 0:
 
 if number_count == 0:
     weak_password_count += 1
-    feedback.append("You haven't used numbers in your password!")    
+    feedback.append("You haven't used any numbers!")  
 
-if weak_password_count > 0:
+if special_character_count == 0:
+    weak_password_count += 1
+    feedback.append("You haven't used special characters!")      
+
+if weak_password_count > strong_password_count:
 
     print("Your password is weak!")
 
-    for f in feedback:
-        print(f)
-        print("\n")
-
-if strong_password_count > 15:
+if strong_password_count <= 8:
     print("Your password is medium!")
 
-if strong_password_count > 20:
+if strong_password_count >= 20:
     print("Your password is very strong!")
+
+for f in feedback:
+        print(f)
+        print("\n")
