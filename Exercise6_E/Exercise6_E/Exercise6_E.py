737b5302bcd5aa85a1ce20b22511f25a4a1f27fc
Andy Bhadai <abhadai@gmail.com>
2015-10-06 20:54:27 +0200

    Circle exercise works

--- Exercise6_E/Exercise6_E/Exercise6_E.py
+++ Exercise6_E/Exercise6_E/Exercise6_E.py
@@ -1,8 +1,27 @@
-﻿size = 30
+﻿import math
+
+# Get user input
+diameter = int(input("Write a number for the width of your circle!"))
+
+# Radius = diameter / 2
+r = diameter / 2
 output = ""
 
-for i in range (0, 10):
+for i in range(0, diameter):
+
+    # Calculate the first middle
+    m1 = (i - (r)) * (i - (r))
+
+    for j in range(0, diameter):
+
+        # Calculate second middle
+        m2 = (j - (r)) * (j - (r))
+
+        # If both middles are smaller than the radius, print and hashtag
+        if math.sqrt(m1 + m2) < r:
+            output += "#"
+        else:
+            output += " "
     output += "\n"
-    for j in range(0, 10):
-        output += "#"
+
 print(output)
